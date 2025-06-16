81c6bda846db3e100278ee7c70f3e74280422d3e
riaz ahmed <106495345+riazku@users.noreply.github.com>
2025-06-16 18:14:51 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,14 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
-    build: {
-        outDir: 'dist', // 👈 Add this line
-        emptyOutDir: true,
-        manifest: true,
-        rollupOptions: {
-            input: 'resources/js/app.js', // or your main entry point
-        },
-    },
-});
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  build: {
+    outDir: 'dist'
+  }
+})
